9bc5e963bfdd18ebfaf4d1305d865d7e6c690811
suanxon <35401240+suanxon@users.noreply.github.com>
2018-01-15 19:30:53 +0700

    Delete 3.py

--- 3.py
+++
@@ -1,7 +0,0 @@
-name = input("What are your name?")
-if name == "Huy be":
-    print("Handsome")
-elif name == "Huy big":
-    even_more_handsome = True
-else:
-    webbrowser.open("https://zing.vn")
